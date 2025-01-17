25645d719182ffedc294fd96195fe69f2800e05e
Aman Vishwakarma <143378283+its-Aman007@users.noreply.github.com>
2025-01-17 15:30:43 +0530

    Update linkedlist.py

--- data-structures/linkedlist.py
+++ data-structures/linkedlist.py
@@ -1,6 +1,6 @@
 class Node: 
   
-    # Constructor to initialize the node object 
+     
     def __init__(self, data): 
         self.data = data 
         self.next = None
@@ -8,11 +8,11 @@ class Node:
   
 class LinkedList: 
   
-    # Function to initialize head 
+    
     def __init__(self): 
         self.head = None
   
-    # Function to reverse the linked list 
+     
     def reverse(self): 
         prev = None
         current = self.head 
@@ -23,13 +23,13 @@ class LinkedList:
             current = next
         self.head = prev 
   
-    # Function to insert a new node at the beginning 
+  
     def push(self, new_data): 
         new_node = Node(new_data) 
         new_node.next = self.head 
         self.head = new_node 
   
-    # Utility function to print the LinkedList 
+    
     def printList(self): 
         temp = self.head 
         while(temp): 
@@ -37,7 +37,7 @@ class LinkedList:
             temp = temp.next
   
   
-# Driver program to test above functions 
+
 llist = LinkedList() 
 llist.push(20) 
 llist.push(4) 
